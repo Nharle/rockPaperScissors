59b7382ecb832cef54eda24e189bcf50d137c56d
Harry Le <88409590+Nharle@users.noreply.github.com>
2026-03-30 08:07:31 -0700

    Created basic UI and printed feedback to window.

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 
 let btns = document.querySelector("#buttons");
-let roundResult = document.querySelector("#roundResult");
-let finalResult = document.querySelector("#finalResult");
-
+let scores = document.querySelector("#scores");
+let roundResult = document.createElement("p");
+let scoreResult = document.createElement("p");
+let finalResult = document.createElement("p");
 let humanScore = 0;
 let computerScore = 0;
+
+scoreResult.textContent = `Score: ${humanScore} - ${computerScore}`;
+
+scores.append(roundResult, scoreResult, finalResult);
+
 function getComputerChoice() {
     let choiceNum = Math.floor(Math.random() * 3);
     if(choiceNum === 0) {
@@ -21,6 +27,7 @@ function playRound(humanChoice, computerChoice) {
     
     if(humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scissors" && computerChoice === "paper") {
         humanScore += 1;
+        scoreResult.textContent = `Score: ${humanScore} - ${computerScore}`;
         roundResult.textContent = `You win the round! ${humanChoice} beats ${computerChoice}! `;
     } 
     else if(humanChoice === computerChoice) {
@@ -28,6 +35,7 @@ function playRound(humanChoice, computerChoice) {
     }
     else {
         computerScore += 1;
+        scoreResult.textContent = `Score: ${humanScore} - ${computerScore}`;
         roundResult.textContent = `You lose the round! ${computerChoice} beats ${humanChoice}!`;
         
     }
